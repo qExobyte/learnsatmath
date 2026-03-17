69c2d797467ce30a465d8c4e043c70d6cd551f38
qExobyte <74025759+qExobyte@users.noreply.github.com>
2026-03-17 16:04:10 -0400

    resources and links setup

--- src/data/masterclassFeatures.ts
+++ src/data/masterclassFeatures.ts
@@ -4,7 +4,7 @@ export const masterclassFeatures = [
   'Recordings of every class included',
   'SAT Math from the Ground Up book included',
   'Practice tests, desmos drills, and 500+ hard problems included',
-  '$100–$500 cash prizes for participation',
+  '$100 - $500 in cash prizes per cohort',
   'Join a community of ambitious students',
   'Most students end with a 750+',
 ];
