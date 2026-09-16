f2c2857f1406a4594ba66c16ddffcbf3ee512a82
qExobyte <eric.wolpert22@gmail.com>
2026-09-16 12:41:39 -0400

    1on1

--- src/data/masterclassFeatures.ts
+++ src/data/masterclassFeatures.ts
@@ -1,7 +1,7 @@
 export const masterclassFeatures = [
   '15 hours of live instruction',
   'Weekly office hours for 1-on-1 questions',
-  'Lifetime access to <img src="/bedrock-pro-logo.svg" alt="" class="inline-block w-5 h-5 align-[-4px]" /> Bedrock Pro; 127 video lessons and 600+ exclusive problems',
+  'Lifetime access to <img src="/bedrock-pro-logo.svg" alt="" class="inline-block w-5 h-5 align-[-4px]" /> Bedrock Pro; 127 video lessons and 800+ exclusive problems',
   'A week-by-week curriculum that has earned 100+ students a 750+',
   'Cash prizes for student performance',
   'Lifetime access to all course materials',
